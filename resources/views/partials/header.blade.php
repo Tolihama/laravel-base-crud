d4bba17afb32be00a01e03df7447a4fa58c73d24
Tolihama <fra070@gmail.com>
2022-01-26 14:13:19 +0100

    Project setup

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    HEADER QUI
+    <nav>
+        <ul>
+            <li>
+                <a href="{{ route('home') }}">Home</a>
+            </li>
+            <li>
+                <a href="{{ route('comics.index') }}">Comic</a>
+            </li>
+        </ul>
+    </nav>
+</header>
